8c96a4431f5d9f1104cb4634adef37e525c0fd1b
khrig <arnssn@gmail.com>
2014-11-25 20:57:13 +0100

    Added source rect to animation

--- Entities/AnimationComponent.cs
+++ Entities/AnimationComponent.cs
@@ -4,6 +4,10 @@ namespace Gengine.Entities {
     public class AnimationComponent : EntityComponent {
         public Rectangle SourceRectangle { get; private set; }
 
+        public AnimationComponent(Rectangle sourceRectangle) {
+            SourceRectangle = sourceRectangle;
+        }
+
         public override void Update(float deltaTime) {
             base.Update(deltaTime);
         }
