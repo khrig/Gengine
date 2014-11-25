14f0152a4b8148ad86ca85127caf8b5abd3b58da
Henrik Aronsson <henrik.aronsson@consortiofashion.com>
2014-11-25 10:25:26 +0100

    Added Renderer

---
+++ Entities/IRenderable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gengine.Entities {
+    public interface IRenderable {
+        Texture2D Texture { get; }
+        Vector2 Position { get; }
+        Rectangle SourceRectangle { get; }
+    }
+}
---
+++ System/Renderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Gengine.Entities;
+
+namespace Gengine.System {
+    public class Renderer {
+        private SpriteBatch spriteBatch;
+
+        public Renderer(SpriteBatch batch) {
+            this.spriteBatch = batch;
+        }
+
+        public void Draw(IEnumerable<IRenderable> list) {
+            foreach (IRenderable renderable in list) {
+                spriteBatch.Draw(
+                    renderable.Texture,
+                    renderable.Position,
+                    renderable.SourceRectangle,
+                    Color.White);
+            }
+        }
+    }
+}
