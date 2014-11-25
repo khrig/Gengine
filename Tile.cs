121eff2cfd8853489aa1d42225b859688e70070d
khrig <arnssn@gmail.com>
2014-11-25 22:17:44 +0100

    Added first version for tiles, needs refactoring

---
+++ Tile.cs
@@ -0,0 +1,17 @@
+﻿using Gengine.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gengine {
+    public class Tile : IRenderable {
+        public Tile(Texture2D texture, Vector2 position, Rectangle sourceRectangle) {
+            Texture = texture;
+            Position = position;
+            SourceRectangle = sourceRectangle;
+        }
+
+        public Texture2D Texture { get; private set; }
+        public Vector2 Position { get; private set; }
+        public Rectangle SourceRectangle { get; private set; }
+    }
+}
---
+++ TileMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Gengine.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gengine {
+    public class TileMap {
+        private List<Tile> tiles;
+
+        // should probably load from file or something
+        public TileMap(Texture2D environmentTexture) {
+            this.tiles = new List<Tile>();
+
+            Vector2 groundPos = new Vector2(0, 480);
+            for (int i = 1; i < 26; i++) {
+                tiles.Add(new Tile(environmentTexture, groundPos, new Rectangle(0, 0, 32, 32)));
+                groundPos.X += 32;
+            }
+        }
+
+        public IEnumerable<IRenderable> Tiles { get { return tiles; } }
+    }
+}
