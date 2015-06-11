a29e0899fd156bd658a22579d0aa4275ab6f56cc
Henrik Aronsson <arnssn@gmail.com>
2015-06-11 19:29:25 +0200

    Changed to center collision for bullets, much better

--- Systems/CollisionSystem.cs
+++ Systems/CollisionSystem.cs
@@ -1,27 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Gengine.Entities;
 using Gengine.Map;
 
 namespace Gengine.Systems {
     public class CollisionSystem {
-        public void HandleCollisions(IEnumerable<ICollidable> collidableObjects) {
-            foreach (var first in collidableObjects) {
-                foreach (var second in collidableObjects) {
+        public void HandleCollisions(IEnumerable<ICollidable> collidableObjects){
+            var collidables = collidableObjects as ICollidable[] ?? collidableObjects.ToArray();
+            foreach (var first in collidables) {
+                foreach (var second in collidables) {
                     TestCollision(first, second);
                 }
             }
         }
 
-        public bool IsColliding(ICollidable collidable, TileMap tileMap){
-            // Find the four tiles we are in
-            Tile t1 = tileMap.PositionToTile(collidable.BoundingBox.Right, collidable.BoundingBox.Top);
-            Tile t2 = tileMap.PositionToTile(collidable.BoundingBox.Right, collidable.BoundingBox.Bottom);
-            Tile t3 = tileMap.PositionToTile(collidable.BoundingBox.Left, collidable.BoundingBox.Top);
-            Tile t4 = tileMap.PositionToTile(collidable.BoundingBox.Left, collidable.BoundingBox.Bottom);
-
-            if (t1.IsSolid || t2.IsSolid || t3.IsSolid || t4.IsSolid)
-                return true;
-            return false;
+        public bool IsCenterColliding(ICollidable collidable, TileMap tileMap){
+            return tileMap.PositionToTile(collidable.BoundingBox.Center.X, collidable.BoundingBox.Center.Y).IsSolid;
         }
 
         public void HandleCollision(ICollidable first, ICollidable second) {
