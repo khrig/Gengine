993ecfe094eaf6db05c5c9888e4c6ea1b83f1cd3
Henrik Aronsson <arnssn@gmail.com>
2015-06-24 23:45:42 +0200

    Updates for component

--- EntityComponentSystem/Entity.cs
+++ EntityComponentSystem/Entity.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 namespace Gengine.EntityComponentSystem {
     public class Entity {
         private const int Maxentities = 4096;
+        private const int MaxComponentsForEntity = 100;
         private static bool[] _entities = new bool[Maxentities];
         private static int _maxComponentId = 0;
-        private static Component[,] _componentToEid = new Component[100, Maxentities];
-
+        private static Component[,] _componentToEid = new Component[Maxentities, MaxComponentsForEntity];
+        
         public static int RegisterComponent(Component component){
             return ++_maxComponentId;
         }
@@ -27,7 +28,7 @@ namespace Gengine.EntityComponentSystem {
         public static int Create(params Component[] components){
             int eid = Create();
             foreach (Component component in components){
-                _componentToEid[component.Id, eid] = component;
+                _componentToEid[eid, component.Id] = component;
             }
             return eid;
         }
@@ -37,14 +38,21 @@ namespace Gengine.EntityComponentSystem {
         }
 
         private Component GetComponent(int eid, int cid) {
-            return _componentToEid[cid, eid];
+            return _componentToEid[eid, cid];
         }
 
-        public static IEnumerable<T> GetAllComponents<T>(int cid) where T : Component{
+        public static IEnumerable<T> GetAllComponents<T>() where T : Component {
+            List<T> res = new List<T>();
             for (int eid = 0; eid < Maxentities; eid++){
-                if (_entities[eid])
-                    yield return (T)_componentToEid[cid, eid];
+                if (_entities[eid]){
+                    for(int i = 0; i < _componentToEid.GetUpperBound(1); i++){
+                        var v = (T) _componentToEid[eid, i];
+                        if(v != null)
+                            res.Add((T)_componentToEid[eid, i]);
+                    }
+                }
             }
+            return res;
         }
     }
 }
