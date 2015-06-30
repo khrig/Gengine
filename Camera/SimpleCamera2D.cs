beca0dbeb6af511d1ca61b7ed9d50da6c41973fe
Henrik Aronsson <arnssn@gmail.com>
2015-06-30 21:29:20 +0200

    Camera update

--- Camera/SimpleCamera2D.cs
+++ Camera/SimpleCamera2D.cs
@@ -3,28 +3,30 @@ using Microsoft.Xna.Framework;
 
 namespace Gengine.Camera {
     public class SimpleCamera2D : ICamera {
-        private readonly IWorld _world;
+        private readonly float _worldWidth;
+        private readonly float _worldHeight;
         private const float ZoomUpperLimit = 1.5f;
         private const float ZoomLowerLimit = .5f;
         private float _zoom;
 
-        public SimpleCamera2D(IWorld world) {
-            _world = world;
+        public SimpleCamera2D(float worldWidth, float worldHeight) {
+            _worldWidth = worldWidth;
+            _worldHeight = worldHeight;
             _position = Vector2.Zero;
             _zoom = 1f;
         }
 
         public void SetPosition(Vector2 position) {
-            _position.X = -(position.X - _world.View.Width / 2);
-            _position.Y = -(position.Y - _world.View.Height / 2);
+            _position.X = -(position.X - _worldWidth / 2);
+            _position.Y = -(position.Y - _worldHeight / 2);
             if (_position.Y < 0)
                 _position.Y = 0;
             if (_position.Y > 0)
                 _position.Y = 0;
             if (_position.X > 0)
                 _position.X = 0;
-            if (_position.X < -_world.View.Width)
-                _position.X = -_world.View.Width;
+            if (_position.X < -_worldWidth)
+                _position.X = -_worldWidth;
         }
 
         private Vector2 _position;
