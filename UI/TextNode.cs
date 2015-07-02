56061749d89e5a85b70d8aed143a11b34d2fba10
Henrik Aronsson <arnssn@gmail.com>
2015-07-02 22:39:44 +0200

    Added node for displaying text

---
+++ UI/TextNode.cs
@@ -0,0 +1,18 @@
+﻿using Gengine.Rendering;
+using Microsoft.Xna.Framework;
+
+namespace Gengine.UI {
+    public class TextNode : IRenderableText {
+        public TextNode(string fontName, string text, Vector2 renderPosition, Color color){
+            FontName = fontName;
+            Text = text;
+            RenderPosition = renderPosition;
+            Color = color;
+        }
+
+        public string FontName { get; private set; }
+        public string Text { get; set; }
+        public Vector2 RenderPosition { get; set; }
+        public Color Color { get; private set; }
+    }
+}
