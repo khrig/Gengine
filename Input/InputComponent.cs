d7482b3a4dce96a1165c88d67470e4977ba806f3
Henrik Aronsson <arnssn@gmail.com>
2015-07-04 16:30:16 +0200

    input component update

--- Input/InputComponent.cs
+++ Input/InputComponent.cs
@@ -1,8 +1,18 @@
-﻿using Gengine.EntityComponentSystem;
+﻿using System;
+using Gengine.EntityComponentSystem;
 
 namespace Gengine.Input {
+    [Flags]
+    public enum InputKey{
+        None = 0,
+        Left = 1,
+        Right = 2,        
+        Up = 4,
+        Down = 8,        
+        Shoot = 16
+    }
+
     public class InputComponent : IComponent {
-        public int DirectionX { get; set; }
-        public int DirectionY { get; set; }
+        public InputKey Input { get; set; }
     }
 }
